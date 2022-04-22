92f49516d51a898e2694ee66aef5c4ea19581890
valentina plastinina <vplastinina@licard.com>
2022-04-22 22:43:07 +0300

    commit

--- task-testing/tests/test_01/test_get_month_days.py
+++ task-testing/tests/test_01/test_get_month_days.py
@@ -1,1 +1,14 @@
+from simple_library_01.functions import get_month_days
 
+
+def test_is_leap():
+    assert 30 == get_month_days(1930, 1)
+    assert 29 == get_month_days(2000, 2)
+    assert 28 == get_month_days(2001, 2)
+    try:
+        get_month_days(2000, 13)
+        assert False
+    except AttributeError:
+        pass
+    assert 30 == get_month_days(2001, 6)
+    assert 31 == get_month_days(2001, 5)
--- task-testing/tests/test_01/test_leap.py
+++ task-testing/tests/test_01/test_leap.py
@@ -1,1 +1,13 @@
+from simple_library_01.functions import is_leap
 
+
+def test_is_leap():
+    assert False == is_leap(2001)
+    assert True == is_leap(2004)
+    assert True == is_leap(2000)
+    assert False == is_leap(2100)
+    try:
+        is_leap(-20)
+        assert False
+    except AttributeError:
+        pass
